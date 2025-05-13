6cabe9ae2000dac9aab81b65bee8639f4a9b7e55
Peddi Shiva <140646940+peddishiva@users.noreply.github.com>
2025-05-13 19:35:34 +0530

    face detection project
    face detection project using openCV

---
+++ openCV/FaceDetection.py
@@ -0,0 +1,34 @@
+import cv2
+
+face_cap = cv2.CascadeClassifier("C:/Users/peddi/AppData/Local/Programs/Python/Python312/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml") #pip install opencv-python is the command used to install opencv
+video_cap = cv2.VideoCapture(0)
+while True:
+    ret , video_data =video_cap.read()
+    col = cv2.cvtColor(video_data, cv2.COLOR_BGR2GRAY)
+    faces = face_cap.detectMultiScale(
+        col, 
+        scaleFactor=1.1, 
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x, y, w, h) in faces:
+        cv2.rectangle(video_data, (x,y), (x+w,y+h), (0, 255, 0),2)
+    cv2.imshow("Live_Face_detection", video_data)
+    if  cv2.waitKey(10) == ord('c'):
+        break
+
+video_cap.release()
+
+''' 
+camera enable code to open.
+video_cap = cv2.VideoCapture(0)
+while True:
+    ret , video_data =video_cap.read()
+    cv2.imshow("Face detection", video_data)
+    if  cv2.waitKey(10) == ord('c'):
+        break
+    
+video_cap.release()
+
+'''
